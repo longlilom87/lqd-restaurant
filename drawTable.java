872d43ed6851d1728c6431de14bdf82aea7688cd
Banh Quang Long <longlilom87@yahoo.com.vn>
2020-05-09 23:10:22 +0700

    Vẽ lại drawTable

--- drawTable.java
+++ drawTable.java
@@ -1,9 +1,7 @@
-
 import java.awt.Choice;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -16,29 +14,65 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+
 
 public class drawTable extends JPanel {
 
 	User user = LoginFrame.user;
-	
+	List<Table> tablelist = getTablelist();
+	JLabel[] tbls = new JLabel[tablelist.size()];
+	JLabel[] name = new JLabel[tablelist.size()];
+	private int x = 400;
+	private int n = 4;
+
 	public drawTable() {
 		setBounds(0,Window.getH()/6,1000,1000);
 		setLayout(null);
 
+		for (int i = 0; i < tablelist.size(); i++) {
+			ImageIcon ic;
+			tbls[i] = new JLabel();
+			tbls[i].setBounds(x - (lineDevision(i, n) - 1) * n * 150, 100 * lineDevision(i, n), 108, 62);
+			name[i] = new JLabel();
+			name[i].setBounds(tbls[i].getX(), tbls[i].getY(), tbls[i].getWidth(), tbls[i].getHeight());
+			if (tablelist.get(i).getY() == 1) {
+				ic = new ImageIcon("Image\\tbl_black.png");
+				name[i].setForeground(Color.white);
+			} else {
+				ic = new ImageIcon("Image\\tbl_green.png");
+				name[i].setForeground(Color.blue);
+			}
+			name[i].setText(String.valueOf(tablelist.get(i).getX()));
+			name[i].setFont(new Font("Tahoma", Font.BOLD, 20));
+
+			name[i].setHorizontalAlignment(SwingConstants.CENTER);
+			name[i].setVerticalAlignment(SwingConstants.CENTER);
+			add(name[i]);
+			Image img = ic.getImage().getScaledInstance(tbls[i].getWidth(), tbls[i].getHeight(), Image.SCALE_SMOOTH);
+			ImageIcon scaledImage = new ImageIcon(img);
+			tbls[i].setIcon(scaledImage);
+			add(tbls[i]);
+
+			x += 150;
+		}
+
 		// Aligning color explanation box
 		int available = 80;
-		int reserved = available+100;
-		int text = 450;
-		int colortext = text-80;
-		
-		int button = 390;
-		int tablechoice=button-200;
-		int ybutton= 330;
+		int reserved = available + 100;
+		int text = 1100;
+		int colortext = text - 80;
+
+		int button = 800;
+		int tablechoice = button - 200;
+		int ybutton = lineDevision(tablelist.size(), n) * 150;
 
 		JLabel lblAvailable = new JLabel("Available");
 		lblAvailable.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -52,7 +86,7 @@ public class drawTable extends JPanel {
 
 		JTextField textField = new JTextField();
 		textField.setForeground(SystemColor.menu);
-		textField.setBackground(Color.GREEN);
+		textField.setBackground(new Color(82, 224, 139));
 		textField.setEditable(false);
 		textField.setBounds(colortext, available, 54, 82);
 		add(textField);
@@ -68,9 +102,9 @@ public class drawTable extends JPanel {
 
 		Choice choice = new Choice();
 		choice.setBounds(tablechoice, ybutton, 123, 25);
+		choice.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		add(choice);
 
-		List<Table> tablelist = getTablelist();
 		for (int i = 0; i < tablelist.size(); i++) {
 			Table t = tablelist.get(i);
 			choice.add(Integer.toString(t.getX()));
@@ -89,28 +123,28 @@ public class drawTable extends JPanel {
 		JButton btnCustomer = new JButton("Choose Table");
 		btnCustomer.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				for(Table t: tablelist) {
-					if(Integer.toString(t.getX()).equals(choice.getSelectedItem())) {
-						if(t.getY()==1) {
+				for (Table t : tablelist) {
+					if (Integer.toString(t.getX()).equals(choice.getSelectedItem())) {
+						if (t.getY() == 1) {
 							JOptionPane.showMessageDialog(null, "Please choose another table");
 							return;
 						}
 					}
 				}
 				try {
-					Menu.bl=true;
+					Menu.bl = true;
 					Window.switchPane(new Menu());
 					user.setTableID(choice.getSelectedItem());
 				} catch (SQLException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				
+
 				update(choice.getSelectedItem(), 1);
 				repaint();
 			}
 		});
-		btnCustomer.setBounds(button, ybutton+30, 97, 25);
+		btnCustomer.setBounds(button, ybutton, 150, 25);
 		add(btnCustomer);
 
 		JButton bDelivery = new JButton("Delivery");
@@ -119,7 +153,7 @@ public class drawTable extends JPanel {
 			public void actionPerformed(ActionEvent e) {
 				Window.homeLayer.removeAll();
 				try {
-					Menu.bl=true;
+					Menu.bl = true;
 					Window.homeLayer.add(new Menu());
 				} catch (SQLException e1) {
 					// TODO Auto-generated catch block
@@ -129,7 +163,7 @@ public class drawTable extends JPanel {
 				Window.homeLayer.revalidate();
 			}
 		});
-		bDelivery.setBounds(button, ybutton+60, 97, 25);
+		bDelivery.setBounds(button, ybutton + 30, 150, 25);
 		add(bDelivery);
 
 	}
@@ -166,58 +200,9 @@ public class drawTable extends JPanel {
 		return tablelist;
 	}
 
-	@Override
-	protected void paintComponent(Graphics g) {
-		List<Table> tablelist = getTablelist();
-
-		int n = 4;
-		int size = 80;
-		int radius = 50;
-
-		super.paintComponent(g);
-		for (Table i : tablelist) {
-
-			int x = i.getX() * size-checkLine(i.getX(),n,size);
-			int y = lineDevision(i.getX(), n) * size;
-			int xString = circleCenter(i.getX(), size, i)-checkLine(i.getX(),n,size);
-			int yString = y + 76;
-
-			if (i.getY() == 0) {
-				g.setColor(Color.green);
-			} else
-				g.setColor(Color.BLACK);
-
-			g.fillOval(x, y, radius, radius);
-			g.setColor(Color.BLUE);
-			Font font = new Font("Verdana", Font.BOLD, 20);
-			g.setFont(font);
-			g.drawString(String.valueOf(i.getX()), xString, yString);
-
-		}
-
-	}
-
-	public int circleCenter(int a, int size, Table t) { // text in the middle below the circle
-		if (a >= 10) {
-			return (int) (a * size + 18 - 6 * Math.log10(t.getX()));
-		} else
-			return a * size + 18;
-	}
-
 	public int lineDevision(int a, int b) {
 		return (int) a / b + 1;
 
 	}
 
-	
-	public int checkLine(int a, int b,int size) {
-		if (a<b) {
-			return 0;
-		}else {
-			
-		     return (lineDevision(a,b)-1)*b*size-size;
-		}
-	}
 }
-
-
