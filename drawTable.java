a43776f18db561a6cbb69b58be8162c76316b627
Banh Quang Long <longlilom87@yahoo.com.vn>
2020-05-10 16:18:13 +0700

    Import Image Again

--- drawTable.java
+++ drawTable.java
@@ -1,3 +1,6 @@
+
+
+import java.awt.CardLayout;
 import java.awt.Choice;
 import java.awt.Color;
 import java.awt.Font;
@@ -5,6 +8,8 @@ import java.awt.Image;
 import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -17,71 +22,44 @@ import java.util.List;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 
-
 public class drawTable extends JPanel {
 
 	User user = LoginFrame.user;
 	List<Table> tablelist = getTablelist();
+	
 	JLabel[] tbls = new JLabel[tablelist.size()];
-	JLabel[] name = new JLabel[tablelist.size()];
+	JLabel[] nameTbl = new JLabel[tablelist.size()];
 	private int x = 400;
 	private int n = 4;
+	static JPanel tableMenu = new JPanel();
 
 	public drawTable() {
-		setBounds(0,Window.getH()/6,1000,1000);
+		setBounds(0, 0, Window.getW(), Window.getH());
 		setLayout(null);
-
-		for (int i = 0; i < tablelist.size(); i++) {
-			ImageIcon ic;
-			tbls[i] = new JLabel();
-			tbls[i].setBounds(x - (lineDevision(i, n) - 1) * n * 150, 100 * lineDevision(i, n), 108, 62);
-			name[i] = new JLabel();
-			name[i].setBounds(tbls[i].getX(), tbls[i].getY(), tbls[i].getWidth(), tbls[i].getHeight());
-			if (tablelist.get(i).getY() == 1) {
-				ic = new ImageIcon("Image\\tbl_black.png");
-				name[i].setForeground(Color.white);
-			} else {
-				ic = new ImageIcon("Image\\tbl_green.png");
-				name[i].setForeground(Color.blue);
-			}
-			name[i].setText(String.valueOf(tablelist.get(i).getX()));
-			name[i].setFont(new Font("Tahoma", Font.BOLD, 20));
-
-			name[i].setHorizontalAlignment(SwingConstants.CENTER);
-			name[i].setVerticalAlignment(SwingConstants.CENTER);
-			add(name[i]);
-			Image img = ic.getImage().getScaledInstance(tbls[i].getWidth(), tbls[i].getHeight(), Image.SCALE_SMOOTH);
-			ImageIcon scaledImage = new ImageIcon(img);
-			tbls[i].setIcon(scaledImage);
-			add(tbls[i]);
-
-			x += 150;
-		}
 
 		// Aligning color explanation box
 		int available = 80;
 		int reserved = available + 100;
-		int text = 1100;
+		int text = 1400;
 		int colortext = text - 80;
-
-		int button = 800;
-		int tablechoice = button - 200;
-		int ybutton = lineDevision(tablelist.size(), n) * 150;
 
 		JLabel lblAvailable = new JLabel("Available");
 		lblAvailable.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblAvailable.setBounds(text, available, 123, 82);
+		lblAvailable.setVisible(true);
 		add(lblAvailable);
 
 		JLabel lblReserved = new JLabel("Serving");
 		lblReserved.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblReserved.setBounds(text, reserved, 123, 82);
+		lblReserved.setVisible(true);
 		add(lblReserved);
 
 		JTextField textField = new JTextField();
@@ -90,7 +68,6 @@ public class drawTable extends JPanel {
 		textField.setEditable(false);
 		textField.setBounds(colortext, available, 54, 82);
 		add(textField);
-		textField.setColumns(10);
 
 		JTextField textField_1 = new JTextField();
 		textField_1.setForeground(SystemColor.menu);
@@ -100,8 +77,11 @@ public class drawTable extends JPanel {
 		textField_1.setBounds(colortext, reserved, 54, 82);
 		add(textField_1);
 
+		int button = 60;
+		int ybutton = 200;
+
 		Choice choice = new Choice();
-		choice.setBounds(tablechoice, ybutton, 123, 25);
+		choice.setBounds(button, ybutton - 40, 150, 25);
 		choice.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		add(choice);
 
@@ -124,7 +104,7 @@ public class drawTable extends JPanel {
 		btnCustomer.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				for (Table t : tablelist) {
-					if (Integer.toString(t.getX()).equals(choice.getSelectedItem())) {
+					if (String.valueOf(t.getX()).equals(choice.getSelectedItem())) {
 						if (t.getY() == 1) {
 							JOptionPane.showMessageDialog(null, "Please choose another table");
 							return;
@@ -166,15 +146,63 @@ public class drawTable extends JPanel {
 		bDelivery.setBounds(button, ybutton + 30, 150, 25);
 		add(bDelivery);
 
+		tableMenu.setLayout(null);
+		tableMenu.setBounds(0, 0, Window.getW() / 4, Window.getH());
+		tableMenu.setBackground(new Color(107, 142, 35));
+
+		JLabel name = new JLabel("LQD");
+		name.setFont(new Font("Harlow Solid Italic", Font.ITALIC, 50));
+		name.setBounds(60, 10, 200, 100);
+		name.setForeground(Color.white);
+		name.setVisible(true);
+		add(name);
+		add(tableMenu);
+
+		JPanel table = new JPanel();
+		table.setBounds(Window.getW() / 8, 0, 1000, Window.getH());
+		table.setLayout(null);
+		table.setVisible(true);
+		add(table);
+
+		for (int i = 0; i < tablelist.size(); i++) {
+			ImageIcon ic;
+			int ID = tablelist.get(i).getX();
+
+			tbls[i] = new JLabel();
+			tbls[i].setBounds(x - (lineDevision(i, n) - 1) * n * 150, 100 * lineDevision(i, n), 108, 62);
+			nameTbl[i] = new JLabel();
+			nameTbl[i].setBounds(tbls[i].getX(), tbls[i].getY(), tbls[i].getWidth(), tbls[i].getHeight());
+
+			if (tablelist.get(i).getY() == 1) {
+				ic = new ImageIcon("Image\\tbl_black.png");
+				nameTbl[i].setForeground(Color.white);
+			} else {
+				ic = new ImageIcon("Image\\tbl_green.png");
+				nameTbl[i].setForeground(Color.blue);
+			}
+			nameTbl[i].setText(String.valueOf(tablelist.get(i).getX()));
+			nameTbl[i].setFont(new Font("Tahoma", Font.BOLD, 20));
+
+			nameTbl[i].setHorizontalAlignment(SwingConstants.CENTER);
+			nameTbl[i].setVerticalAlignment(SwingConstants.CENTER);
+			table.add(nameTbl[i]);
+			Image img = ic.getImage().getScaledInstance(tbls[i].getWidth(), tbls[i].getHeight(), Image.SCALE_SMOOTH);
+			ImageIcon scaledImage = new ImageIcon(img);
+			tbls[i].setIcon(scaledImage);
+			table.add(tbls[i]);
+			x += 150;
+
+		}
+
 	}
 
 	public void update(String id, int n) {
 		try {
-			Class.forName("org.sqlite.JDBC");
-			Connection c = DriverManager.getConnection("jdbc:sqlite:restaurant.db");
+			Connection c = Menu.Connect();
 			String update = "update Res_table set Table_status=" + n + " where TableID=" + id;
 			PreparedStatement st = c.prepareStatement(update);
 			st.executeUpdate();
+			c.close();
 
 		} catch (Exception err1) {
 			System.out.println(err1);
