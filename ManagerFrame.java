8d5b84259d41683db544b9028cada845f9476681
RestauGroup <64661534+RestauGroup@users.noreply.github.com>
2020-05-10 14:24:20 +0700

    Create ManagerFrame.java

---
+++ ManagerFrame.java
@@ -0,0 +1,353 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import java.sql.*;
+import javax.swing.*;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.Font;
+import javax.swing.border.BevelBorder;
+import javax.swing.SwingConstants;
+import javax.swing.JTable;
+import javax.swing.border.LineBorder;
+
+import net.proteanit.sql.DbUtils;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ManagerFrame extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtAboutUs;
+	private JTextField txtEmployee;
+	private JTextField txtMenu;
+	private JTextField txtTable;
+	private JTextField txtBill;
+	private JTextField textField;
+	public static JTable table_2;
+	private JPanel panel_1;
+	private JPanel panel_2;
+	private JButton btnNewButton;
+	private JButton btnNewButton_1;
+	private JButton btnNewButton_2;
+	private JButton btnNewButton_3;
+	private JButton btnNewButton_4;
+	private JButton btnNewButton_5;
+
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) throws SQLException { 
+		Connection conne = Connect();
+//		PreparedStatement create = conne.prepareStatement("CREATE TABLE IF NOT EXISTS Res_table (" + 
+//				"     tableID int PRIMARY KEY,\r\n" + 
+//				"	  Table_status int,\r\n );");
+//		create.executeUpdate();
+		PreparedStatement stmt = conne.prepareStatement("SELECT * FROM Res_table;");
+//		ResultSet rs = stmt.executeQuery();
+
+
+
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ManagerFrame frame = new ManagerFrame();
+					frame.setVisible(true);
+					frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	
+	public static Connection Connect() throws SQLException {
+		Connection connection = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			String url = "jdbc:sqlite:Restaurant.db";
+			connection = DriverManager.getConnection(url);
+			System.out.println("ConnectJDBC");
+		} catch (ClassNotFoundException e) {
+			System.out.println("ERROR :"+e.getMessage()+"/n"+e.getClass()+"/n"+e.getCause());
+			e.printStackTrace();
+		}
+		return connection;
+	}
+
+
+
+	/**
+	 * Create the frame.
+	 */
+	public ManagerFrame() {
+	
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0, 0, 1366, 768);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 250, 205));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(107, 142, 35));
+		panel.setBounds(0, 0, 1370, 103);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Welcome .....  ");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel.setBounds(10, 112, 158, 25);
+		contentPane.add(lblNewLabel);
+		
+		txtAboutUs = new JTextField();
+		txtAboutUs.addMouseListener(new MouseAdapter() {
+			
+			public void mouseClicked(MouseEvent e) {
+				btnNewButton_1.setVisible(true);
+				btnNewButton.setVisible(false);
+
+				try {
+				Connection connection= Connect();
+				String query = "SELECT * FROM Authentication_Login;";
+				PreparedStatement	stmt = connection.prepareStatement(query);
+				ResultSet rs1 = stmt.executeQuery();
+				table_2.setModel(DbUtils.resultSetToTableModel(rs1));
+			} catch (SQLException e1) {
+					
+					e1.printStackTrace();
+				
+			
+			}}
+			
+		});
+		txtAboutUs.setEditable(false);
+		txtAboutUs.setBackground(Color.LIGHT_GRAY);
+		txtAboutUs.setHorizontalAlignment(SwingConstants.CENTER);
+		txtAboutUs.setText("Customer");
+		txtAboutUs.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		txtAboutUs.setBounds(1220, 114, 140, 33);
+		contentPane.add(txtAboutUs);
+		txtAboutUs.setColumns(10);
+		
+		txtEmployee = new JTextField();
+		txtEmployee.addMouseListener(new MouseAdapter() {
+			
+			public void mouseClicked(MouseEvent e) {
+				btnNewButton_2.setVisible(true);
+				btnNewButton_3.setVisible(true);
+				btnNewButton.setVisible(false);
+				btnNewButton_1.setVisible(false);
+				table_2.setVisible(false);
+
+
+				
+			}
+		});
+		txtEmployee.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		txtEmployee.setBackground(Color.LIGHT_GRAY);
+		txtEmployee.setEditable(false);
+		txtEmployee.setText("Staff");
+		txtEmployee.setHorizontalAlignment(SwingConstants.CENTER);
+		txtEmployee.setBounds(1060, 114, 140, 33);
+		contentPane.add(txtEmployee);
+		txtEmployee.setColumns(10);
+		
+		txtMenu = new JTextField();
+		txtMenu.setEditable(false);
+		txtMenu.setText("Menu");
+		txtMenu.setHorizontalAlignment(SwingConstants.CENTER);
+		txtMenu.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		txtMenu.setBackground(Color.LIGHT_GRAY);
+		txtMenu.setBounds(901, 114, 140, 33);
+		contentPane.add(txtMenu);
+		txtMenu.setColumns(10);
+		
+		txtTable = new JTextField();
+		txtTable.addMouseListener(new MouseAdapter() {
+			
+			public void mouseClicked(MouseEvent e) {
+				btnNewButton.setVisible(true);
+				btnNewButton_1.setVisible(false);
+				try {
+					Connection connection= Connect();
+					String query = "SELECT * FROM Res_table;";
+				 PreparedStatement	stmt = connection.prepareStatement(query);
+					ResultSet rs = stmt.executeQuery();
+					table_2.setModel(DbUtils.resultSetToTableModel(rs));
+				
+					
+				} catch (SQLException e1) {
+					
+					e1.printStackTrace();
+				}
+			
+				
+				
+				
+				
+			}
+		});
+		txtTable.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		txtTable.setEditable(false);
+		txtTable.setHorizontalAlignment(SwingConstants.CENTER);
+		txtTable.setBackground(Color.LIGHT_GRAY);
+		txtTable.setForeground(Color.BLACK);
+		txtTable.setText("Table ");
+		txtTable.setBounds(739, 114, 140, 33);
+		contentPane.add(txtTable);
+		txtTable.setColumns(10);
+		
+		txtBill = new JTextField();
+		txtBill.setEditable(false);
+		txtBill.setHorizontalAlignment(SwingConstants.CENTER);
+		txtBill.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		txtBill.setText("Bill");
+		txtBill.setBackground(Color.LIGHT_GRAY);
+		txtBill.setBounds(574, 114, 140, 33);
+		contentPane.add(txtBill);
+		txtBill.setColumns(10);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(34, 220, 986, 412);
+		contentPane.add(scrollPane);
+		
+		table_2 = new JTable();
+		scrollPane.setViewportView(table_2);
+		
+		panel_2 = new JPanel();
+		panel_2.setBackground(new Color(255, 250, 205));
+		panel_2.setBounds(1031, 220, 257, 412);
+		contentPane.add(panel_2);
+		panel_2.setLayout(null);
+		
+		btnNewButton = new JButton("Up Table");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Uptable uptable;
+				try {
+					uptable = new Uptable();
+					uptable.setVisible(true);
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		btnNewButton.setBounds(55, 65, 109, 99);
+		panel_2.add(btnNewButton);
+		btnNewButton.setVisible(false);
+		
+		
+		btnNewButton_1 = new JButton("Up cus");
+		btnNewButton_1.addMouseListener(new MouseAdapter() {
+			
+			public void mouseClicked(MouseEvent e) {
+				Upcustomer upcus;
+				upcus = new Upcustomer();
+				upcus.setVisible(true);
+			}
+			
+		});
+		btnNewButton_1.setBounds(55, 65, 109, 99);
+		panel_2.add(btnNewButton_1);
+		btnNewButton_1.setVisible(false);
+		
+		btnNewButton_4 = new JButton("Up Chef");
+		btnNewButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				UpChef upchef;
+				upchef = new UpChef();
+				upchef.setVisible(true);
+			}
+		});
+		btnNewButton_4.setBounds(57, 65, 107, 99);
+		panel_2.add(btnNewButton_4);
+		btnNewButton_4.setVisible(false);
+		
+		btnNewButton_5 = new JButton("Add New Partner");
+		btnNewButton_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				UpManager upmana;
+				upmana = new UpManager();
+				upmana.setVisible(true);
+			}
+		});
+		btnNewButton_5.setBounds(55, 65, 109, 99);
+		panel_2.add(btnNewButton_5);
+		btnNewButton_5.setVisible(false);
+		
+		
+	    btnNewButton_2 = new JButton("Manager");
+	    btnNewButton_2.addActionListener(new ActionListener() {
+	    	public void actionPerformed(ActionEvent e) {
+	    		btnNewButton_5.setVisible(true);
+	    		btnNewButton_4.setVisible(false);
+	    		table_2.setVisible(true);
+
+				try {
+
+	    		Connection connection= Connect();
+
+				String query = "SELECT * FROM Authentication_Login Where Login_Role ='Manager' ;";
+
+	    		PreparedStatement	stmt = connection.prepareStatement(query);
+	    		ResultSet rs3 = stmt.executeQuery();
+	    		table_2.setModel(DbUtils.resultSetToTableModel(rs3));
+	    	} catch (SQLException e3) {
+	    		e3.printStackTrace();
+
+	    		
+	    	}}
+	    });
+		btnNewButton_2.setBounds(34, 186, 89, 23);
+		contentPane.add(btnNewButton_2);
+		btnNewButton_2.setVisible(false);
+		
+		
+		btnNewButton_3 = new JButton("Chef");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnNewButton_4.setVisible(true);
+				btnNewButton_5.setVisible(false);
+				table_2.setVisible(true);
+
+				try {
+					Connection connection= Connect();
+					String query = "SELECT * FROM Authentication_Login Where Login_Role ='Chef' ;";
+					PreparedStatement	stmt = connection.prepareStatement(query);
+					ResultSet rs2 = stmt.executeQuery();
+					table_2.setModel(DbUtils.resultSetToTableModel(rs2));
+				} catch (SQLException e2) {
+					e2.printStackTrace();
+
+				}}
+		});
+		btnNewButton_3.setBounds(148, 186, 89, 23);
+		contentPane.add(btnNewButton_3);
+		btnNewButton_3.setVisible(false);
+
+		
+		
+		
+		
+		
+	
+		
+		
+		
+		
+	}
+}
