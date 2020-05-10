7781427d3b378072912c1c111633f8c5062be59a
quyenhp15 <quyenhp15@gmail.com>
2020-05-10 14:52:05 +0700

    Decor LoginFrame

--- LoginFrame.java
+++ LoginFrame.java
@@ -13,6 +13,7 @@ import java.awt.event.ActionEvent;
 import javax.swing.border.BevelBorder;
 
 import java.awt.Font;
+import java.awt.Image;
 
 import javax.swing.SwingConstants;
 import javax.swing.JLabel;
@@ -21,6 +22,7 @@ import javax.swing.JPanel;
 import javax.swing.ImageIcon;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.sql.*;
 import java.util.ArrayList;
 
@@ -36,6 +38,13 @@ public class LoginFrame extends JFrame{
 
 	public static User user;
 	static ArrayList<User> arr;
+	private ImageIcon icon = new ImageIcon("Image/LoginFrame.jpg"); 
+	private Image scale = icon.getImage().getScaledInstance(500, 300, java.awt.Image.SCALE_SMOOTH);
+	private Image usernameImage = new ImageIcon("Image/username.png").getImage().getScaledInstance(50,50, java.awt.Image.SCALE_SMOOTH);
+	private Image passwordImage = new ImageIcon("Image/password.png").getImage().getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH);
+	private Image xImage = new ImageIcon("Image/X.png").getImage().getScaledInstance(50,50, java.awt.Image.SCALE_SMOOTH);
+	private JLabel X;
+	
 //	public static void main(String[] args) throws SQLException {
 //		Connection c = Connect();
 //		PreparedStatement create = c.prepareStatement("CREATE TABLE IF NOT EXISTS Authentication_Login (" + 
@@ -102,27 +111,6 @@ public class LoginFrame extends JFrame{
 	
 	public LoginFrame() throws SQLException {
 		initialize();
-		
-//		Connection c = Connect();
-//		PreparedStatement create = c.prepareStatement("CREATE TABLE IF NOT EXISTS Authentication_Login (" + 
-//				"     username varchar(100)  Unique,\r\n" + 
-//				"	 userpassword varchar(14),\r\n" + 
-//				"	 Login_Role varchar(20 ),"
-//				+ "name varchar(100),"
-//				+ " );");
-//		create.executeUpdate();
-		
-//		PreparedStatement insert = c.prepareStatement("INSERT INTO Authentication_Login (username, userpassword, Login_Role) values ('QuinLeaderr156', '1234231', 'Manager'),('DuckDuck', 'anhiuem123', 'Chef'),('Q_Dragon', 'nhantobanh', 'Customer');\r\n" + 
-//				"");
-//		insert.executeUpdate();
-		
-//		PreparedStatement stmt = c.prepareStatement("SELECT * FROM Authentication_Login;");
-//		ResultSet r = stmt.executeQuery();
-//		while(r.next()) {
-//			System.out.println(r.getString("username")+" "+r.getString("userpassword")+" "+r.getString("Login_Role"));
-//		}
-		
-//		check();
 	}
 
 	/**
@@ -131,12 +119,48 @@ public class LoginFrame extends JFrame{
 	private void initialize() {
 		setBackground(new Color(255, 0, 255));
 		getContentPane().setBackground(new Color(192, 192, 192));
-		setBounds(450, 40, 500, 300);
+		setBounds(450, Window.getH()/2-200, 500, 300);
+		setResizable(false);
+		setUndecorated(true);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		getContentPane().setLayout(null);
 		
+		X = new JLabel(new ImageIcon(xImage));
+		X.setBounds(440, 11, 50,50);
+		X.addMouseListener(new MouseListener() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				dispose();
+			}
+		});
+		getContentPane().add(X);
+		
 		username = new JTextField();
-		username.setBounds(158, 75, 219, 29);
+		username.setBounds(106, 106, 219, 29);
 		getContentPane().add(username);
 		username.setColumns(10);
 		
@@ -150,10 +174,12 @@ public class LoginFrame extends JFrame{
 				}
 			}
 		});
-		password.setBounds(158, 115, 219, 29);
+		password.setBounds(106, 159, 219, 29);
 		getContentPane().add(password);
 		
 		txtLogin = new JTextField();
+		txtLogin.setBackground(new Color(0, 128, 0));
+		txtLogin.setForeground(new Color(255, 255, 255));
 		txtLogin.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -170,15 +196,17 @@ public class LoginFrame extends JFrame{
 			}
 		});
 		txtLogin.setHorizontalAlignment(SwingConstants.CENTER);
-		txtLogin.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		txtLogin.setFont(new Font("Tahoma", Font.BOLD, 20));
 		txtLogin.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
 		txtLogin.setEditable(false);
 		txtLogin.setText("Register");
-		txtLogin.setBounds(158, 173, 103, 20);
+		txtLogin.setBounds(120, 233, 103, 29);
 		getContentPane().add(txtLogin);
 		txtLogin.setColumns(10);
 	
 		txtLogin_1 = new JTextField();
+		txtLogin_1.setForeground(new Color(255, 255, 255));
+		txtLogin_1.setBackground(new Color(0, 128, 0));
 		txtLogin_1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -188,36 +216,35 @@ public class LoginFrame extends JFrame{
 		txtLogin_1.setHorizontalAlignment(SwingConstants.CENTER);
 		txtLogin_1.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
 		txtLogin_1.setEditable(false);
-		txtLogin_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		txtLogin_1.setFont(new Font("Tahoma", Font.BOLD, 20));
 		txtLogin_1.setText("Login");
-		txtLogin_1.setBounds(282, 173, 97, 20);
+		txtLogin_1.setBounds(290, 233, 97, 29);
 		getContentPane().add(txtLogin_1);
 		txtLogin_1.setColumns(10);
 		
-		JLabel Users = new JLabel("User");
+		JLabel Users = new JLabel(new ImageIcon(usernameImage));
 		Users.setFont(new Font("Tahoma", Font.BOLD, 12));
 		Users.setHorizontalAlignment(SwingConstants.LEFT);
 		Users.setBackground(new Color(175, 238, 238));
-		Users.setBounds(44, 75, 71, 29);
+		Users.setBounds(34, 94, 50, 50);
 		getContentPane().add(Users);
 		
-		JLabel lblNewLabel = new JLabel("RESTAURANT");
-		lblNewLabel.setForeground(new Color(255, 255, 255));
+		JLabel lblNewLabel = new JLabel("LOGIN");
+		lblNewLabel.setForeground(Color.WHITE);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 27));
-		lblNewLabel.setBounds(169, 26, 187, 38);
+		lblNewLabel.setFont(new Font("Berlin Sans FB Demi", Font.BOLD | Font.ITALIC, 70));
+		lblNewLabel.setBounds(10, 11, 239, 67);
 		getContentPane().add(lblNewLabel);
 		
-		Password = new JLabel("Password");
+		Password = new JLabel(new ImageIcon(passwordImage));
 		Password.setFont(new Font("Tahoma", Font.BOLD, 12));
 		Password.setHorizontalAlignment(SwingConstants.LEFT);
-		Password.setBounds(44, 115, 71, 29);
+		Password.setBounds(34, 155, 50, 54);
 		getContentPane().add(Password);
 		
-		JLabel Validation = new JLabel("");
-		Validation.setForeground(new Color(255, 255, 255));
-		Validation.setBounds(158, 155, 219, 14);
-		getContentPane().add(Validation);
+		JLabel bg = new JLabel(new ImageIcon(scale));
+		bg.setBounds(0, 0, 500, 300);
+		getContentPane().add(bg);
 	}
 	
 	public void login() {
@@ -245,7 +272,4 @@ public class LoginFrame extends JFrame{
 			e1.printStackTrace();
 		}
 	}
-	
-//	public void Change() {
-//	}
 }
