454be0d26c79ce8a3a72ae6e02db9b04ef0b80b9
Banh Quang Long <longlilom87@yahoo.com.vn>
2020-05-11 18:40:52 +0700

    Update UpdateMenu.java
    Thêm add, update

--- UpdateMenu.java
+++ UpdateMenu.java
@@ -25,10 +25,8 @@ import javax.swing.table.JTableHeader;
 import javax.swing.table.TableColumn;
 
 
-
 public class UpdateMenu extends JPanel {
 
-	
 	Choice choice = new Choice();
 	ArrayList<Food> food;
 	String[] column = { "ID", "Name", "Unit Price" };
@@ -44,7 +42,7 @@ public class UpdateMenu extends JPanel {
 		setBounds(0, 0, Window.getW(), Window.getH());
 		setLayout(null);
 		setBackground(new Color(255, 250, 205));
-		
+
 		choice.setBounds(700, 150, 300, 40);
 		choice.setFont(new Font("Tahoma", Font.PLAIN, 40));
 		choice.addItem("Burger");
@@ -53,12 +51,13 @@ public class UpdateMenu extends JPanel {
 		choice.addItem("Pizza");
 		add(choice);
 
-		JLabel instruction = new JLabel ("Please click on Go to see the Table");
+		JLabel instruction = new JLabel("Please click on Go to see the Table");
 		instruction.setBounds(1200, 150, 300, 55);
-		instruction.setFont(new Font("Arial",Font.ITALIC,15));
+		instruction.setFont(new Font("Arial", Font.ITALIC, 15));
 		instruction.setForeground(Color.red);
 		add(instruction);
-		
+		releaseTable();
+
 		JButton bGo = new JButton("Go!");
 		bGo.setBounds(1050, 150, 60, 55);
 		bGo.setVisible(true);
@@ -67,90 +66,194 @@ public class UpdateMenu extends JPanel {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				instruction.setVisible(false);
-				menuTable = new JTable(createTableModel()) {
+				releaseTable();
 
-					@Override
-					public boolean isCellEditable(int row, int column) {
-						return false;
-					}
-
-				};
-				menuTable.setFont(new Font("Tahoma", Font.PLAIN, 30));
-				menuTable.getTableHeader().setFont(new Font("SansSerif", Font.BOLD, 30));
-				menuTable.setRowHeight(60);
-
-				for (int i = 0; i < column.length; i++) {
-					JTableHeader header = menuTable.getTableHeader();
-					TableColumn col = header.getColumnModel().getColumn(i);
-					col.setHeaderValue(column[i]);
-					header.repaint();
-				}
-
-				JScrollPane js = new JScrollPane(menuTable);
-				js.setBounds(0, 230, Window.getW() - 20, 400);
-				js.setVisible(true);
-				add(js);
 			}
 
 		});
 		add(bGo);
 
-	
 		JLabel lblDeleteItem = new JLabel("Delete Item");
 		lblDeleteItem.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		lblDeleteItem.setBounds(1200, 650, 218, 59);
 		lblDeleteItem.setHorizontalAlignment(JLabel.CENTER);
 		add(lblDeleteItem);
-		
-	
+
 		JTextField textField = new JTextField();
 		textField.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		textField.setBounds(lblDeleteItem.getX()+115, lblDeleteItem.getY()+85, 146, 34);
+		textField.setBounds(lblDeleteItem.getX() + 115, lblDeleteItem.getY() + 85, 146, 34);
 		add(textField);
 		textField.setColumns(10);
-		
+
 		JLabel lblNewLabel = new JLabel("Enter Item ID");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNewLabel.setBounds(lblDeleteItem.getX()-31, lblDeleteItem.getY()+94, 105, 16);
+		lblNewLabel.setBounds(lblDeleteItem.getX() - 31, lblDeleteItem.getY() + 94, 105, 16);
 		add(lblNewLabel);
-		
+
 		JButton btnDelete = new JButton("Delete");
 		btnDelete.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnDelete.setBounds(lblDeleteItem.getX()+44, lblDeleteItem.getY()+154, 134, 42);
+		btnDelete.setBounds(lblDeleteItem.getX() + 44, lblDeleteItem.getY() + 154, 134, 42);
 		btnDelete.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				try {
-					Connection c= Menu.Connect(); 
-					String sql_1 = "Select idFood from Menu where idFood= '"+textField.getText()+"' ";
-					PreparedStatement s1= c.prepareStatement(sql_1);
-					ResultSet rs= s1.executeQuery();
-					if (rs.next()==true) {
-						String sql = "Delete from Menu where id = '"+textField.getText()+"' ";
-						PreparedStatement s2= c.prepareStatement(sql);
-						s2.executeUpdate();
-						for (Food i: food ) {
-							if (i.getId().equals(textField.getText())&& menuTable.isVisible()==true){
-								menuTable.repaint();
-							}
+					Connection c = Menu.Connect();
+					int check = selectID(textField.getText());
+					if (check == 1) {
+						JOptionPane.showMessageDialog(null, "No food " + textField.getText(), "Alert",
+								JOptionPane.WARNING_MESSAGE);
+					} else {
+
+						if (textField.getText().isEmpty()) {
+							JOptionPane.showMessageDialog(null, "Please input ID", "Error", JOptionPane.ERROR_MESSAGE);
+						} else {
+							String sql = "Delete from Menu where idFood = '" + textField.getText() + "' ";
+							PreparedStatement s2 = c.prepareStatement(sql);
+							s2.executeUpdate();
+							menuTable.setModel(createTableModel());
+							refreshHeader();
 						}
-							
-					}else JOptionPane.showMessageDialog(null,"No food "+textField.getText(),"Alert",JOptionPane.WARNING_MESSAGE);
-					
-					
-					c.close();
+
+					}
+
 				} catch (SQLException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
+
 			}
-			
+
 		});
 		add(btnDelete);
-		
-		
+
+		JLabel lblUpdateItem = new JLabel("Update Item");
+		lblUpdateItem.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		lblUpdateItem.setHorizontalAlignment(JLabel.CENTER);
+		lblUpdateItem.setBounds(400, 650, 209, 69);
+		add(lblUpdateItem);
+
+		JLabel lblEnterName = new JLabel("Enter Item Name");
+		lblEnterName.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblEnterName.setBounds(lblUpdateItem.getX() - 90, lblUpdateItem.getY() + 82, 190, 35);
+		add(lblEnterName);
+
+		JLabel lblEnterPrice = new JLabel("Enter Item Price");
+		lblEnterPrice.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblEnterPrice.setBounds(lblEnterName.getX(), lblEnterName.getY() + 60, 190, 35);
+		add(lblEnterPrice);
+
+		JTextField txtEnterName = new JTextField();
+		txtEnterName.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		txtEnterName.setBounds(lblEnterName.getX() + 203, lblEnterName.getY(), 183, 31);
+		add(txtEnterName);
+		txtEnterName.setColumns(10);
+
+		JTextField txtEnterPrice = new JTextField();
+		txtEnterPrice.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		txtEnterPrice.setColumns(10);
+		txtEnterPrice.setBounds(txtEnterName.getX(), lblEnterPrice.getY(), 183, 31);
+		add(txtEnterPrice);
+
+		JLabel lblEnterID = new JLabel("Enter Item ID");
+		lblEnterID.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblEnterID.setBounds(lblEnterName.getX(), lblEnterName.getY() + 120, 190, 35);
+		add(lblEnterID);
+
+		JTextField txtEnterID = new JTextField();
+		txtEnterID.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		txtEnterID.setColumns(10);
+		txtEnterID.setBounds(txtEnterName.getX(), lblEnterID.getY(), 183, 31);
+		add(txtEnterID);
+
+		JButton btnAdd = new JButton("Add");
+		btnAdd.setBounds(lblUpdateItem.getX() - 43, lblUpdateItem.getY() + 260, 97, 25);
+		btnAdd.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+
+				if (txtEnterID.getText().isEmpty() || txtEnterName.getText().isEmpty()
+						|| txtEnterPrice.getText().isEmpty()) {
+
+					JOptionPane.showMessageDialog(null, "You have to input all the fields", "Error",
+							JOptionPane.ERROR_MESSAGE);
+				} else {
+					System.out.println("txtenter = " + txtEnterID.getText());
+					int dialogbutton = JOptionPane.showConfirmDialog(null,
+							"Are you sure? \n You can't edit the ID anymore!", "WARNING", JOptionPane.YES_NO_OPTION);
+
+					if (dialogbutton == JOptionPane.YES_OPTION) {
+						try {
+							Connection c = Menu.Connect();
+							int check = selectID(txtEnterID.getText());
+							if (check == 2) {
+								JOptionPane.showMessageDialog(null, "Food existed. \n You can only update", "Error",
+										JOptionPane.ERROR_MESSAGE);
+							} else {
+								String iFood = "insert into Menu (idFood, name, price) values('" + txtEnterID.getText()
+										+ "','" + txtEnterName.getText() + "','" + txtEnterPrice.getText() + "')";
+								PreparedStatement s = c.prepareStatement(iFood);
+								s.executeUpdate();
+								menuTable.setModel(createTableModel());
+								refreshHeader();
+							}
+
+						} catch (SQLException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+
+					}
+				}
+
+			}
+
+		});
+		add(btnAdd);
+
+		JButton btnUpdate = new JButton("Update");
+		btnUpdate.setBounds(lblUpdateItem.getX() + 147, btnAdd.getY(), 97, 25);
+		btnUpdate.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				Connection c = null;
+				try {
+					c = Menu.Connect();
+					String update = "", name = "", price = "";
+					if (txtEnterName.getText().isEmpty() == false) {
+						name = "name ='" + txtEnterName.getText() + "' ";
+					}
+
+					if (txtEnterPrice.getText().isEmpty() == false) {
+						price = "price ='" + txtEnterPrice.getText() + "' ";
+					}
+					if (txtEnterName.getText().isEmpty() == false && txtEnterPrice.getText().isEmpty() == false) {
+						update = ",";
+					}
+					int check = selectID(txtEnterID.getText());
+					if (check == 1) {
+						JOptionPane.showMessageDialog(null, "No food " + textField.getText(), "Alert",
+								JOptionPane.WARNING_MESSAGE);
+					} else {
+						String sql = "update Menu set " + name + update + price + " where idFood = '"
+								+ txtEnterID.getText() + "'";
+						PreparedStatement s = c.prepareStatement(sql);
+						s.executeUpdate();
+						menuTable.setModel(createTableModel());
+						refreshHeader();
+					}
+
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+		});
+		add(btnUpdate);
+
 	}
 
 	private ArrayList<Food> getMenu(Choice choice) {
@@ -208,6 +311,58 @@ public class UpdateMenu extends JPanel {
 
 		return model;
 
+	}
+
+	private void refreshHeader() {
+		for (int i = 0; i < column.length; i++) {
+			JTableHeader header = menuTable.getTableHeader();
+			TableColumn col = header.getColumnModel().getColumn(i);
+			col.setHeaderValue(column[i]);
+			header.repaint();
+		}
+	}
+
+	private void releaseTable() {
+		menuTable = new JTable(createTableModel()) {
+
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+
+		};
+		menuTable.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		menuTable.getTableHeader().setFont(new Font("SansSerif", Font.BOLD, 30));
+		menuTable.setRowHeight(60);
+
+		refreshHeader();
+
+		JScrollPane js = new JScrollPane(menuTable);
+		js.setBounds(0, 230, Window.getW() - 20, 400);
+		js.setVisible(true);
+		add(js);
+
+	}
+
+	private int selectID(String ID) {
+		Connection c = null;
+		int i = 0;
+		try {
+			c = Menu.Connect();
+			String getID = "SELECT idFood FROM Menu WHERE idFood ='" + ID + "'";
+			PreparedStatement s = c.prepareStatement(getID);
+			ResultSet result = s.executeQuery();
+			if (result.next() == true) {
+				i = 2;
+
+			} else
+				i = 1;
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return i;
 	}
 
 }
